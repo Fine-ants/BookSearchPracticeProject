f4cb07396cfaedbb0de1c1283dcaac05a96f003e
Jordan Huus <jordanhuusy@live.com>
2018-05-10 18:15:48 -0700

    implemented adapter into search button onClickListener

--- app/src/main/java/com/example/jorda/booksearchpracticeproject/BookSearchActivity.java
+++ app/src/main/java/com/example/jorda/booksearchpracticeproject/BookSearchActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -34,11 +35,9 @@ public class BookSearchActivity extends AppCompatActivity {
 
                 // Fetch books matching user input from Google Books API
                 ArrayList<Book> books = Utils.fetchVolumesFromJson(bookSearchInput);
-                for(Book book: books){
-                    Log.i("debugtag", book.getAuthor());
-                    Log.i("debugtag", book.getTitle());
-                    Log.i("debugtag", book.getPrice()+"");
-                }
+                BookAdapter adapter = new BookAdapter(BookSearchActivity.this, R.layout.list_item, books);
+                ListView booksListView = findViewById(R.id.books_list_view);
+                booksListView.setAdapter(adapter);
             }
         });
 
